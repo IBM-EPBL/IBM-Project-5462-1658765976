1c0f0238ff1d54a3704beb84f17bf6c040a8de6e
KavinP0103 <112748419+KavinP0103@users.noreply.github.com>
2022-09-26 21:39:55 +0530

    Update assignment2.py

--- Assignments/Kavin P/Assignment-2/assignment2.py
+++ Assignments/Kavin P/Assignment-2/assignment2.py
@@ -1,22 +1,34 @@
 import random
 
-def send_alert(msg):
+def alert(msg):
     print(msg)
 
 def read_humidity():
     return random.randint(10, 100)
 
 def read_temperature():
-    return random.randint(0, 100)
+    return random.randint(-100, 100)
     
-def monitor_temp_humid():
+def start_monitoring():
     while True:
         temperature = read_temperature()
         humidity = read_humidity()
         if temperature >= 40:
-            send_alert("High Temperature Detected")
-        if humidity >= 50:
-            send_alert("High Humidity Detected")
+            alert("Temperature is High")
+        elif temperature <= 25 and temperature >= 10:
+            alert("Temperature is Low")
+        elif temperature <= 10:
+            alert("Temperature is Very Low")
+        else:
+            alert("Normal Temperature")
+        if humidity > 80:
+            alert("Humidity is Very High")
+        elif humidity > 50:
+            alert("Humidity is High")
+        elif humidity < 30:
+            alert("Humidity is Low")
+        else:
+            alert("Moderate Humidity")
             
 if __name__ == "__main__":
-    monitor_temp_humid()
+    start_monitoring()
